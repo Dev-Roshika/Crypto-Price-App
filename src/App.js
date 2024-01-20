110bda4b7a0e91679a9c8df12815f198d0264c90
Dev-Roshika <roshikanayanadhara@gmail.com>
2024-01-20 21:23:56 +0530

    . error resoloved

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
-import React, {useReducer} from "react";
-import './styles.css'
-import DigitButton from './DigitButton'
-import OperationButton from './OperationButton'
+import React, { useReducer } from "react";
+import "./styles.css";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
 
 export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DEGIT: 'delete-digit',
-  EVALUATE: 'evaluate'
-}
-const reducer = (state, {type, payload}) => {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DEGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+const reducer = (state, { type, payload }) => {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if(payload.digit === '0' && state.currOp === '0' ) return state
+      if (payload.digit === "0" && state.currOp === "0") return state;
+      if (payload.digit === "." && state.currOp.includes(".")) return state;
       return {
         ...state,
-        currOp: `${state.currOp || ''}${payload.digit}`
-      }
+        currOp: `${state.currOp || ""}${payload.digit}`,
+      };
   }
-}
+};
 function App() {
-  const [{currOp, prevOp, Operation}, dispatch] = useReducer(reducer, {});
+  const [{ currOp, prevOp, Operation }, dispatch] = useReducer(reducer, {});
 
   return (
     <div className="cal-grid">
       <div className="output">
-        <div className="prev-op">{prevOp}{Operation}</div>
+        <div className="prev-op">
+          {prevOp}
+          {Operation}
+        </div>
         <div className="curr-op">{currOp}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <OperationButton operation='÷' dispatch={dispatch} />
-      <DigitButton digit='1' dispatch={dispatch} />
-      <DigitButton digit='2' dispatch={dispatch} />
-      <DigitButton digit='3' dispatch={dispatch} />
-      <OperationButton operation='*' dispatch={dispatch} />
-      <DigitButton digit='4' dispatch={dispatch} />
-      <DigitButton digit='5' dispatch={dispatch} />
-      <DigitButton digit='6' dispatch={dispatch} />
-      <OperationButton operation='+' dispatch={dispatch} />
-      <DigitButton digit='7' dispatch={dispatch} />
-      <DigitButton digit='8' dispatch={dispatch} />
-      <DigitButton digit='9' dispatch={dispatch} />
-      <OperationButton operation='-' dispatch={dispatch} />
-      <DigitButton digit='.' dispatch={dispatch} />
-      <DigitButton digit='0' dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
